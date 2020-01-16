0b0c132385b19de1d2309c82afc0e1447ee8d829
Naresh Charugundla <nachar@microsoft.com>
2020-01-15 23:37:16 -0800

    Commiting Account class and methods

---
+++ BankApp/BankApp/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    /// <summary>
+    /// This Account class is to represent a bank account. It contains all the properties that a normal bank account has.
+    /// </summary>
+    class Account
+    {
+        #region Properties
+        public int AccountNumber { get; set; }
+        public string AccountName { get; set; }
+        public string AccountType { get; set; }
+        public decimal Balance { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime CreatedDate { get; set; }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Deposit money into the account
+        /// </summary>
+        /// <param name="Amount">Amount to deposit</param>
+        public void Deposit(decimal Amount)
+        {
+            Balance += Amount;
+        }
+
+        public decimal WithDraw(decimal Amount)
+        {
+            Balance -= Amount;
+            return Balance;
+        }
+
+        #endregion
+    }
+}
--- BankApp/BankApp/Program.cs
+++ BankApp/BankApp/Program.cs
@@ -6,7 +6,11 @@ namespace BankApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var myAccount = new Account();
+            myAccount.AccountNumber = 123;
+            myAccount.AccountName = "Checking";
+            myAccount.Deposit(100);
+            Console.WriteLine($"AN: {myAccount.AccountNumber}, AccountBalance: {myAccount.Balance}, CreatedDate: {myAccount.CreatedDate}");
         }
     }
 }
